01d290d0d1b833007b59eaa143d2016892d0b151
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2023-04-14 15:08:19 +0200

    Ajout des commentaires pour les constructeurs et destructeur

--- PT_CPP_Matrices/PT_CPP_Matrices/CMatrice.h
+++ PT_CPP_Matrices/PT_CPP_Matrices/CMatrice.h
@@ -10,9 +10,54 @@ template<class MTYPE> class CMatrice
 		MTYPE** pMATMatrice;
 
 	public:
+		/***********************************************************************
+		***** CMATRICE<MTYPE> : Constructeur par défaut de CMatrice<MTYPE> *****
+		************************************************************************
+		***** Entrée :                                                     *****
+		***** Nécessite :                                                  *****
+		***** Sortie :                                                     *****
+		***** Entraine : Le contructeur a initialisé un objet              *****
+		***** CMatrice<MTYPE> avec uiMATNbLignes et uiMATNbColonnes égaux  *****
+		***** à 0 et pMATMatrice à NULL                                    *****
+		***********************************************************************/
 		CMatrice<MTYPE>();
+
+		/***********************************************************************
+		***** CMATRICE<MTYPE> : Constructeur de recopie de CMatrice<MTYPE> *****
+		************************************************************************
+		***** Entrée : MATParam, un objet de type CMatrice<MTYPE>          *****
+		***** Nécessite :                                                  *****
+		***** Sortie :                                                     *****
+		***** Entraine : Le contructeur a initialisé un objet              *****
+		***** CMatrice<MTYPE> correspondant à une copie de l'objet         *****
+		***** MATParam                                                     *****
+		***********************************************************************/
 		CMatrice<MTYPE>(CMatrice<MTYPE> &MATParam);
+
+		/***********************************************************************
+		***** CMATRICE<MTYPE> : Constructeur de comfort de CMatrice<MTYPE> *****
+		************************************************************************
+		***** Entrée : - uiNbLignes, entier non signé, nombre de lignes de *****
+		***** la matrice à créer                                           *****
+		***** - uiNbColonnes, entier non signé, nombre de colonnes de la   *****
+		***** matrice à créer                                              *****
+		***** Nécessite : uiNbLignes et uiNbColonnes entiers positifs      *****
+		***** Sortie :                                                     *****
+		***** Entraine : Le contructeur a initialisé un objet              *****
+		***** CMatrice<MTYPE> comportant uiNbLignes et uiNbColonnes        *****
+		***********************************************************************/
 		CMatrice<MTYPE>(unsigned int uiNbLignes, unsigned int uiNbColonnes);
+
+		/************************************************************
+		***** ~CMATRICE<MTYPE> : Destructeur de CMatrice<MTYPE> *****
+		*************************************************************
+		***** Entrée :                                          *****
+		***** Nécessite :                                       *****
+		***** Sortie :                                          *****
+		***** Entraine : Le destructeur a désalloué tous les    *****
+		***** éléments de la matrice en mettant leur valeur à   *****
+		***** NULL                                              *****
+		************************************************************/
 		~CMatrice<MTYPE>();
 
 		int MATLireNbLignes();
@@ -21,7 +66,12 @@ template<class MTYPE> class CMatrice
 		void MATModifierElement(unsigned int uiIndiceLigne, unsigned int uiIndiceColonne, MTYPE valeur);
 		CMatrice<MTYPE>& operator=(CMatrice<MTYPE> &MATParam);
 		CMatrice<MTYPE>& operator*(int iParam);
-
+		CMatrice<MTYPE>& operator/(int iParam);
+		void MATAfficher();
+		CMatrice<MTYPE>& MATTransposer();
+		CMatrice<MTYPE>& operator+(CMatrice<MTYPE> &MATParam);
+		CMatrice<MTYPE>& operator-(CMatrice<MTYPE> &MATParam);
+		CMatrice<MTYPE>& operator*(CMatrice<MTYPE> &MATParam);
 
 };
 
