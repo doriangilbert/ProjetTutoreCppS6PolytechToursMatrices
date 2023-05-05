6ebfd3e4a8953b363bd9e020d9732c6d4685d173
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2023-05-05 14:33:16 +0200

    Correction d'une erreur Visual Studio fonction deprecated (C4996)

--- PT_CPP_Matrices/PT_CPP_Matrices/CLecteur.h
+++ PT_CPP_Matrices/PT_CPP_Matrices/CLecteur.h
@@ -7,6 +7,8 @@
 #include <cstring>
 using namespace std;
 
+#pragma warning(disable : 4996) //Ligne utilisée pour éviter l'erreur Visual Studio : C4996 This function or variable may be unsafe. Consider using fopen_s instead. To disable deprecation, use _CRT_SECURE_NO_WARNINGS
+
 #define NULL 0;
 
 class CLecteur 
