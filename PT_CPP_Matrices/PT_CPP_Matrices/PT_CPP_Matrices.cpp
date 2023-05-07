6a38103d6106fa34cfd2efda98139eb353ca776f
Mathh267 <mathh267@gmail.com>
2023-05-07 12:51:02 +0200

    Update Application principale

--- PT_CPP_Matrices/PT_CPP_Matrices/PT_CPP_Matrices.cpp
+++ PT_CPP_Matrices/PT_CPP_Matrices/PT_CPP_Matrices.cpp
@@ -11,76 +11,73 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	//TODO : Vérifier la valeur de argc (argc > 1) et lire argv si la condition est remplie (y'a toujours le nom du fichier enfaite)
-	if (argc > 1) {
-		CLecteur * LECLecteur = new CLecteur(argv[1]);
-		CMatrice<double> MATMatrice = LECLecteur->LECLireFichierMatrice();
-		MATMatrice.MATAfficher();
-	}
-	
- 	CMatrice<double> LISMATListeMatrice[5];
-	for (int uiBoucleIndiceListeMatrice = 0; uiBoucleIndiceListeMatrice < 5; uiBoucleIndiceListeMatrice++) {
-		LISMATListeMatrice[uiBoucleIndiceListeMatrice] = *(new CMatrice<double>(2, 2));
-		for (unsigned int uiBoucleIndiceLigne = 0; uiBoucleIndiceLigne < 2; uiBoucleIndiceLigne++) {
-			for (unsigned int uiBoucleIndiceColonne = 0; uiBoucleIndiceColonne < 2;uiBoucleIndiceColonne++) {
-				LISMATListeMatrice[uiBoucleIndiceListeMatrice].MATModifierElement(uiBoucleIndiceLigne, uiBoucleIndiceColonne, uiBoucleIndiceLigne + uiBoucleIndiceColonne + uiBoucleIndiceListeMatrice);
+	int iNombreParametres = (argc - 1);
+	cout << iNombreParametres << "\n";
+	if (iNombreParametres > 0) {
+		CMatrice<double>* LISMATListeMatrice1 = new CMatrice<double>[iNombreParametres];
+		for (int iBoucle = 1; iBoucle < argc; iBoucle++) {
+			CLecteur* LECLecteur = new CLecteur(argv[iBoucle]);
+			CMatrice<double> MATMatrice = LECLecteur->LECLireFichierMatrice();
+			LISMATListeMatrice1[iBoucle - 1] = MATMatrice;
+			cout << "Resultat M" << iBoucle << "\n";
+			(LISMATListeMatrice1[iBoucle - 1]).MATAfficher();
+		}
+		cout << "Entrer un entier \n";
+		int c;
+		if (cin >> c) {
+			cout << "C'est un entier: " << c << endl;
+			try {
+				for (int uiBoucleIndiceListeMatrice = 0; uiBoucleIndiceListeMatrice < iNombreParametres; uiBoucleIndiceListeMatrice++) {
+					cout << "Resultat M" << uiBoucleIndiceListeMatrice + 1 << " * c \n";
+					(LISMATListeMatrice1[uiBoucleIndiceListeMatrice] * c).MATAfficher();
+				}
+				for (int uiBoucleIndiceListeMatrice = 0; uiBoucleIndiceListeMatrice < iNombreParametres; uiBoucleIndiceListeMatrice++) {
+					cout << "Resultat M" << uiBoucleIndiceListeMatrice + 1 << " / c \n";
+					(LISMATListeMatrice1[uiBoucleIndiceListeMatrice] / c).MATAfficher();
+				}
 			}
-		}
-		cout << "Resultat M"<< uiBoucleIndiceListeMatrice <<"\n";
-		LISMATListeMatrice[uiBoucleIndiceListeMatrice].MATAfficher();
-	}
-	//LISMATListeMatrice[2] = *(new CMatrice<double>(4, 2));
-	//for (unsigned int uiBoucleIndiceLigne = 0; uiBoucleIndiceLigne < 4; uiBoucleIndiceLigne++) {
-	//	for (unsigned int uiBoucleIndiceColonne = 0; uiBoucleIndiceColonne < 2; uiBoucleIndiceColonne++) {
-	//		LISMATListeMatrice[2].MATModifierElement(i, uiBoucleIndiceColonne, uiBoucleIndiceLigne + uiBoucleIndiceColonne + 2);
-	//	}
-	//}
-	cout << "Entrer un entier \n";
-	int c;
-	if (cin >> c) {
-		cout << "C'est un entier: " << c << endl;
-		for (int uiBoucleIndiceListeMatrice = 0; uiBoucleIndiceListeMatrice < 5; uiBoucleIndiceListeMatrice++) {
-			cout << "Resultat M" << uiBoucleIndiceListeMatrice << " * c \n";
-			(LISMATListeMatrice[uiBoucleIndiceListeMatrice] * c).MATAfficher();
-		}
-		for (int uiBoucleIndiceListeMatrice = 0; uiBoucleIndiceListeMatrice < 5; uiBoucleIndiceListeMatrice++) {
-			cout << "Resultat M" << uiBoucleIndiceListeMatrice << " * c \n";
-			(LISMATListeMatrice[uiBoucleIndiceListeMatrice] / c).MATAfficher();
-		}
+			catch (CException EXCErreur) {
+				if (EXCErreur.EXCLireValeur() == DivisionPar0) cout << "Division par 0\n";
+			}
 
-	}
-	else cout << "Ce n'est pas un entier" << endl;
-	cout << "Resultat MSomme\n";
-	try {
-		CMatrice<double> MSomme(LISMATListeMatrice[0]);
-		for (int uiBoucleIndiceListeMatrice = 1; uiBoucleIndiceListeMatrice < 5; uiBoucleIndiceListeMatrice++) {
-			MSomme = MSomme + LISMATListeMatrice[uiBoucleIndiceListeMatrice];
 		}
-		MSomme.MATAfficher();
-		CMatrice<double> MSommeBizarre(LISMATListeMatrice[0]);
-		for (int uiBoucleIndiceListeMatrice = 1; uiBoucleIndiceListeMatrice < 5; uiBoucleIndiceListeMatrice++) {
-			if (uiBoucleIndiceListeMatrice % 2 != 0) {
-				MSommeBizarre = MSommeBizarre - LISMATListeMatrice[uiBoucleIndiceListeMatrice];
+		else cout << "Ce n'est pas un entier" << endl;
+		cout << "Resultat MSomme\n";
+		try {
+			CMatrice<double> MSomme(LISMATListeMatrice1[0]);
+			for (int uiBoucleIndiceListeMatrice = 1; uiBoucleIndiceListeMatrice < iNombreParametres; uiBoucleIndiceListeMatrice++) {
+				MSomme = MSomme + LISMATListeMatrice1[uiBoucleIndiceListeMatrice];
 			}
-			else {
-				MSommeBizarre = MSommeBizarre + LISMATListeMatrice[uiBoucleIndiceListeMatrice];
+			MSomme.MATAfficher();
+			CMatrice<double> MSommeBizarre(LISMATListeMatrice1[0]);
+			for (int uiBoucleIndiceListeMatrice = 1; uiBoucleIndiceListeMatrice < iNombreParametres; uiBoucleIndiceListeMatrice++) {
+				if (uiBoucleIndiceListeMatrice % 2 != 0) {
+					MSommeBizarre = MSommeBizarre - LISMATListeMatrice1[uiBoucleIndiceListeMatrice];
+				}
+				else {
+					MSommeBizarre = MSommeBizarre + LISMATListeMatrice1[uiBoucleIndiceListeMatrice];
+				}
 			}
+			cout << "Resultat MSommeBizarre\n";
+			MSommeBizarre.MATAfficher();
 		}
-		cout << "Resultat MSommeBizarre\n";
-		MSommeBizarre.MATAfficher();
-	}
-	catch (CException EXCErreur) {
-		cout << "Erreur Taille de Matrice non conforme\n";
-	}
-	cout << "Resultat MProduit\n";
-	try {
-		CMatrice<double> MProduit(LISMATListeMatrice[0]);
-		for (int uiBoucleIndiceListeMatrice = 1; uiBoucleIndiceListeMatrice < 5; uiBoucleIndiceListeMatrice++) {
-			MProduit = MProduit * LISMATListeMatrice[uiBoucleIndiceListeMatrice];
+		catch (CException EXCErreur) {
+			cout << "Erreur Taille de Matrice non conforme\n";
 		}
-		MProduit.MATAfficher();
+		cout << "Resultat MProduit\n";
+		try {
+			CMatrice<double> MProduit(LISMATListeMatrice1[0]);
+			for (int uiBoucleIndiceListeMatrice = 1; uiBoucleIndiceListeMatrice < iNombreParametres; uiBoucleIndiceListeMatrice++) {
+				MProduit = MProduit * LISMATListeMatrice1[uiBoucleIndiceListeMatrice];
+			}
+			MProduit.MATAfficher();
+		}
+		catch (CException EXCErreur) {
+			cout << "Erreur Taille de Matrice non conforme\n";
+		}
 	}
-	catch(CException EXCErreur){
-		cout << "Erreur Taille de Matrice non conforme\n";
+	else {
+		cout << "Pas de paramètre \n";
 	}
 	
 }
